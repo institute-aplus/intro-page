b65b7627fca1a949eacfc1576549bede5bb42aaf
institute-aplus <122430311+institute-aplus@users.noreply.github.com>
2024-02-01 17:21:45 +0000

    remove next files

--- _next/static/ShvSeOROIu_BSChz9wklb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/453-605571afe91c0b80.js","static/css/b2fdbc4ca8de6557.css","static/chunks/pages/index-e5e22a99d60ee69b.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/chunks/328.1244db07bc10b813.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[328],{2740:function(){},7665:function(){},5491:function(){},4622:function(){},9071:function(){}}]);
